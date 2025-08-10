7b1cbfaf31f9485865e1cc967e81e73c92d6fc0f
bibinvijayan01 <bibinvijayan2018@gmail.com>
2025-08-10 15:27:36 +0530

    home page template integration

---
+++ Kidoozaapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'Homepage/index.html')
+
+
+def user(request):
+     return render(request,"customers/index.html")
+
